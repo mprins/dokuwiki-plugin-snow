6b1bb6fcb03fdaa5bb7ffae6aeeabbf0ffd196f9
En Matt <heartattack@free.fr>
2019-02-15 15:20:10 +0100

    translation update

---
+++ lang/oc/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author En Matt <heartattack@free.fr>
+ */
+$lang['enabled']               = 'Activar o desactivar la nèu';
