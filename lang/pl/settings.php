e9926b9e7e20da03e25ad0549728d3ba65ad7482
DokuWiki Translation Tool <admin@dokuwiki.org>
2026-04-19 14:13:50 +0200

    Translation update (pl) (#42)

--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ *
  * language file for snow plugin.
  *
  * Copyright (c) 2013 Mark C. Prins <mprins@users.sf.net>
@@ -17,7 +18,20 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
+ * @author Marek Adamski <fevbew@wp.pl>
  * @author Bartek S <sadupl@gmail.com>
  * @author Mark C. Prins <mprins@users.sf.net>
  */
 $lang['enabled']               = 'Włącz lub wyłącz śnieg';
+$lang['zindex']                = 'Kolejność układania CSS zastosowana do każdego płatka śniegu';
+$lang['color']                 = 'Nie jedz (ani nie używaj?) żółtego śniegu.';
+$lang['excludemobile']         = 'Śnieg może być złą wiadomością dla procesorów (i baterii) telefonów komórkowych. Włączasz tę funkcję na własne ryzyko.';
+$lang['flakesmax']             = 'Ogranicz całkowitą ilość wytwarzanego śniegu (opadania i przyklejania się)';
+$lang['flakesmaxactive']       = 'Ogranicz ilość śniegu spadającego jednocześnie (mniej = mniejsze obciążenie procesora)';
+$lang['animationinterval']     = 'Teoretyczny pomiar „milisekund na klatkę”. 20 = szybko i płynnie, ale duże obciążenie procesora. 50 = bardziej zachowawczo, ale wolniej.';
+$lang['usegpu']                = 'Włącz przyspieszenie sprzętowe oparte na transformacji, zmniejsz obciążenie procesora.';
+$lang['followmouse']           = 'Ruch śniegu może reagować na ruchy myszy użytkownika';
+$lang['snowstick']             = 'Czy śnieg powinien „przyklejać się” do dna? Po wyłączeniu nigdy się nie zbierze.';
+$lang['usemelteffect']         = 'Podczas recyklingu opadłego śniegu (lub rzadziej, gdy pada), pozwól mu się „roztopić” i zniknąć, jeśli przeglądarka to obsługuje';
+$lang['usetwinkleeffect']      = 'Pozwól, aby śnieg losowo pojawiał się i znikał z pola widzenia podczas spadania';
+$lang['freezeonblur']          = 'Śnieg pojawia się tylko wtedy, gdy okno jest aktywne (na pierwszym planie). Oszczędza procesor.';
